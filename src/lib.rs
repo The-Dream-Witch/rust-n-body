87ffd9595dae4b61fa54802a9b38bf6844c86a29
The-Dream-Witch <mhawke@pdx.edu>
2021-06-05 23:02:51 -0700

    Managed to clear up most of the tree issues, but it seems like the algorithm chokes pretty hard during the initial singularity; I'm unsure if this is an issue with my implementation, the need for some fine tuning, or something else. Will look into this further, but can't dedicate too much more time to it given that there are other things I have to take care of on this project before the due date, and the tree was really just an exercise in autodidactic gratification. Will likely continue tweaking the project once the term is over, though.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 extern crate rand;
 
-use std::ops;
 use std::default::Default;
-
+use std::ops;
 
 const G: f64 = 6.67e-11;
-const DT: f64 = 100000.;
+const DT: f64 = 1000.;
 
-pub const XMAX: f64 = 800.;
-pub const YMAX: f64 = 800.;
+pub const XMAX: f64 = 1920.;
+pub const YMAX: f64 = 1080.;
 pub const ZMAX: f64 = 800.;
-pub const EPSILON: f64 = 0.;
-pub const THETA: f64 = 0.3;
 
-#[derive(Clone,Debug)]
+pub const SOFTENING: f64 = 500.;
+pub const THETA: f64 = 0.5;
+
+#[derive(Clone, Debug)]
 pub struct Nbodies {
     pub bodies: Vec<Body>,
     pub tree: Vec<OctTree>,
@@ -24,60 +24,64 @@ impl Nbodies {
         let mut newbodies: Vec<Body> = Vec::new();
 
         for _ in 0..n {
-            let newbody = Body::new();
-            newbodies.push(newbody);
+
+            newbodies.push(Body::new());
         }
-        Self {bodies: newbodies, tree: Vec::with_capacity(1)}
+
+        Self {
+            bodies: newbodies,
+            tree: Vec::with_capacity(1),
+        }
     }
 
     pub fn update_with_tree(&mut self) {
-        println!("Num_bodies: {}", self.bodies.len());
         self.tree.push(OctTree::new(XMAX, Vec3D::default()));
-        
-        for i in 0..self.bodies.len()-1 {
+
+        for i in 0..self.bodies.len() - 1 {
             self.tree[0].find_subtree(self.bodies[i].clone());
         }
 
-        for i in 0..self.bodies.len()-1 {
+        for i in 0..self.bodies.len() - 1 {
             self.tree[0].update_body(&mut self.bodies[i]);
+            self.bodies[i].bounds();
         }
 
         self.tree.clear();
     }
-    
+
     pub fn update_naive(&mut self) {
         let mut delta_pos: Vec<Vec3D> = Vec::new();
         let mut mags: Vec<f64> = Vec::new();
         let mut k = 0;
 
-        for i in 0..self.bodies.len()-1 {
+        for i in 0..self.bodies.len() - 1 {
             let mass1 = self.bodies[i].mass;
-            for j in i+1..self.bodies.len() {
+            for j in i + 1..self.bodies.len() {
                 let mass2 = self.bodies[j].mass;
 
                 //Calculate and store differences between particle positions
-                delta_pos.push(self.bodies[i].pos-self.bodies[j].pos);
-                
+                delta_pos.push(self.bodies[i].pos - self.bodies[j].pos);
+
                 //Calculate and store magnitudes
                 mags.push(delta_pos[i].get_scalar());
-                
+
                 //Calculate and apply new velocities
                 self.bodies[i].vel -= delta_pos[k] * (mass2 * mags[k]) * G;
                 self.bodies[j].vel += delta_pos[k] * (mass1 * mags[k]) * G;
-                k +=1;
+                k += 1;
             }
         }
-    
+
         //Compute new positions
         for i in 0..self.bodies.len() {
             let vel = self.bodies[i].vel;
             self.bodies[i].pos += vel * DT;
-            self.bodies[i].pos.bounds();
+            self.bodies[i].bounds();
         }
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Vec3D(pub f64, pub f64, pub f64);
 
 impl Vec3D {
@@ -87,44 +91,28 @@ impl Vec3D {
 
     fn get_scalar(&self) -> f64 {
         let sumsqr = self.sum_sqrs();
-        DT / ((sumsqr * sumsqr).sqrt())
+        DT / ((sumsqr).sqrt())
     }
 
     pub fn new() -> Self {
         use rand::Rng;
         let mut rng = rand::thread_rng();
-    
-        Self(rng.gen_range(0.0..800.0), rng.gen_range(0.0..800.0), rng.gen_range(0.0..800.0))
+
+        Self(
+            rng.gen_range(0.0..XMAX),
+            rng.gen_range(0.0..YMAX),
+            rng.gen_range(0.0..ZMAX),
+        )
     }
 
-    pub fn new_with_tuple((x,y,z): (f64,f64,f64)) -> Self {
-        Self(x,y,z)
-    }
-
-    pub fn bounds(&mut self) {
-        if self.0 < 0. {
-           self.0 = 800.;
-        } else if self.0 > 800. {
-            self.0 = 0.;
-        }
-
-        if self.1 < 0. {
-            self.1 = 800.;
-        } else if self.1 > 800. {
-            self.1 = 0.;
-        }
-
-        if self.2 < 0. {
-            self.2 = 800.;
-        } else if self.2 > 800. {
-            self.2 = 0.;
-        }
+    pub fn new_with_tuple((x, y, z): (f64, f64, f64)) -> Self {
+        Self(x, y, z)
     }
 }
 
 impl Default for Vec3D {
     fn default() -> Vec3D {
-        Vec3D(0.,0.,0.)
+        Vec3D(0., 0., 0.)
     }
 }
 
@@ -172,11 +160,11 @@ impl ops::SubAssign for Vec3D {
     }
 }
 
-impl PartialEq for Vec3D {
+/*impl PartialEq for Vec3D {
     fn eq(&self, rhs: &Self) -> bool {
         self.0 == rhs.0 && self.1 == rhs.1 && self.2 == rhs.2
     }
-}
+}*/
 
 impl ops::Mul<Vec3D> for Vec3D {
     type Output = Vec3D;
@@ -185,7 +173,7 @@ impl ops::Mul<Vec3D> for Vec3D {
     }
 }
 
-#[derive(Clone, Debug, Copy)]
+#[derive(Clone, Debug, Copy, PartialEq)]
 pub struct Body {
     pub pos: Vec3D,
     pub vel: Vec3D,
@@ -197,11 +185,42 @@ impl Body {
         use rand::Rng;
         let mut rng = rand::thread_rng();
 
-        Self{pos: Vec3D::new(), vel: Vec3D::default(), mass: rng.gen_range(1..10) as f64}
+        Self {
+            pos: Vec3D::new(),
+            vel: Vec3D::default(),
+            mass: rng.gen_range(1..100) as f64,
+        }
+        
     }
 
     pub fn equal(&self, rhs: &Self) -> bool {
         self.pos == rhs.pos && self.vel == rhs.vel && self.mass == rhs.mass
+    }
+
+    pub fn bounds(&mut self) {
+        if self.pos.0 < 0. {
+            self.pos.0 = 0.;
+            self.vel.0 = self.vel.0 * -1.;
+        } else if self.pos.0 > XMAX {
+            self.pos.0 = XMAX;
+            self.vel.0 = self.vel.0 * -1.;
+        }
+
+        if self.pos.1 < 0. {
+            self.pos.1 = 0.;
+            self.vel.1 = self.vel.1 * -1.;
+        } else if self.pos.1 > YMAX {
+            self.pos.1 = YMAX;
+            self.vel.1 = self.vel.1 * -1.;
+        }
+
+        if self.pos.2 < 0. {
+            self.pos.2 = 0.;
+            self.vel.2 = self.vel.2 * -1.;
+        } else if self.pos.2 > ZMAX {
+            self.pos.2 = ZMAX;
+            self.vel.2 = self.vel.2 * -1.;
+        }
     }
 }
 
@@ -220,70 +239,94 @@ pub struct OctTree {
 
     pub center: Vec3D,
     pub center_mass: Vec3D,
-    
+
     pub total_mass: f64,
     pub width: f64,
     pub num_bodies: u32,
 }
 
 impl OctTree {
-    pub fn new(width: f64, center: Vec3D) -> Self {     
-        Self {current_body: Vec::with_capacity(1), sub_trees: Vec::with_capacity(8), center, center_mass: center, total_mass: 0.0, width, num_bodies: 0}
+    pub fn new(width: f64, center: Vec3D) -> Self {
+        Self {
+            current_body: Vec::with_capacity(1),
+            sub_trees: Vec::with_capacity(8),
+            center,
+            center_mass: center,
+            total_mass: 0.0,
+            width,
+            num_bodies: 0,
+        }
     }
 
     pub fn add_body(&mut self, body: Body) {
-        self.center_mass = (self.center_mass * self.total_mass + body.pos * body.mass) * (1. / (self.total_mass + body.mass));
+        self.center_mass = (self.center_mass * self.total_mass + body.pos * body.mass)
+            * (1. / (self.total_mass + body.mass));
         self.total_mass += body.mass;
-        
+
         self.num_bodies += 1;
 
         if self.num_bodies == 1 {
             self.current_body.push(body);
         } else {
             self.find_subtree(body);
-            
+
             if self.num_bodies == 2 {
                 let body_to_find = self.current_body.pop().unwrap();
                 self.find_subtree(body_to_find);
             }
-        } 
+        }
     }
 
     pub fn find_subtree(&mut self, body: Body) {
-        let coord_mod = [(1.,1.,1.),(1.,1.,-1.),(1.,-1.,1.),(1.,-1.,-1.),(-1.,1.,1.),(-1.,1.,-1.),(-1.,-1.,1.),(-1.,-1.,-1.)].to_vec();
-        
+        if self.width / 2. < 1. {
+            return;
+        }
+
+        let coord_mod = [
+            (1., 1., 1.),
+            (1., 1., -1.),
+            (1., -1., 1.),
+            (1., -1., -1.),
+            (-1., 1., 1.),
+            (-1., 1., -1.),
+            (-1., -1., 1.),
+            (-1., -1., -1.),
+        ]
+        .to_vec();
+
         if self.sub_trees.is_empty() {
             for modifier in coord_mod {
                 let mut sub_tree_center = Vec3D::new_with_tuple(modifier);
-                sub_tree_center = sub_tree_center*(self.center+(self.width/4.));
-                
-                self.sub_trees.push(OctTree::new(self.width / 2., sub_tree_center));
+                sub_tree_center = sub_tree_center * (self.center + (self.width / 4.));
+
+                self.sub_trees
+                    .push(OctTree::new(self.width / 2., sub_tree_center));
             }
         }
 
         for i in 0..8 {
-            if f64::abs(self.sub_trees[i].center.0 - body.pos.0) <= (self.sub_trees[i].width / 2.)
-            && f64::abs(self.sub_trees[i].center.1 - body.pos.1) <= (self.sub_trees[i].width / 2.)
-            && f64::abs(self.sub_trees[i].center.2 - body.pos.2) <= (self.sub_trees[i].width / 2.) {
+            if f64::abs(self.sub_trees[i].center.0 - body.pos.0) <= self.sub_trees[i].width
+                && f64::abs(self.sub_trees[i].center.1 - body.pos.1)
+                    <= self.sub_trees[i].width
+                && f64::abs(self.sub_trees[i].center.2 - body.pos.2)
+                    <= self.sub_trees[i].width
+            {
                 self.sub_trees[i].add_body(body);
-            } 
+            }
         }
     }
 
     pub fn update_body(&self, body: &mut Body) {
         let delta_pos = self.center_mass - body.pos;
         let distance_squared = delta_pos.sum_sqrs();
-        if (self.num_bodies == 1 && !(body.equal(&self.current_body[0]))) || self.width * self.width < THETA * THETA * distance_squared {
-            //let distance = (distance_squared + EPSILON * EPSILON).sqrt();
+        if (self.num_bodies == 1 && *body != self.current_body[0])
+            || ((self.width * self.width) < (THETA * THETA * distance_squared))
+        {
             let mag = delta_pos.get_scalar();
-            let norm_acc = G * self.total_mass * mag;
 
-            body.vel.0 += delta_pos.0 * norm_acc;
-            body.vel.1 += delta_pos.1 * norm_acc;
-            body.vel.2 += delta_pos.2 * norm_acc;
-            
-            body.pos += body.vel * DT;
-            body.pos.bounds();
+            body.vel += delta_pos * self.total_mass * mag * G;
+            body.pos += body.vel;
+
         } else if !self.sub_trees.is_empty() {
             for sub_tree in &self.sub_trees {
                 if sub_tree.num_bodies != 0 {
@@ -293,15 +336,3 @@ impl OctTree {
         }
     }
 }
-/*
-            let mass1 = self.total_mass;
-            let mass2 = body.mass;
-
-            let mag = delta_pos.get_scalar();
-                    
-            body.vel += delta_pos * (mass1 * mag) * G;
-        
-            let vel = body.vel;
-            body.pos += vel * DT;
-            body.pos.bounds();
-*/
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ extern crate opengl_graphics;
 extern crate piston;
 
 use glutin_window::GlutinWindow as Window;
+use n_body_sim::*;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent};
 use piston::window::WindowSettings;
-use n_body_sim::*;
 
 pub struct Sim {
     gl: GlGraphics, // OpenGL drawing backend.
@@ -16,28 +16,33 @@ pub struct Sim {
 
 impl Sim {
     fn render(&mut self, args: &RenderArgs, nbodies: &Nbodies) {
-        use graphics::*;
         use graphics::color::*;
-        
-        
+        use graphics::*;
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(BLACK, gl);
             //Draw each body to the screen in its updated position
             for body in &nbodies.bodies {
-                let transform = c
-                    .transform
-                    .trans(body.pos.0, body.pos.1);
-                    
-                    let size = (body.pos.2/80.).ceil();
-                    let circle = ellipse::circle(0.0, 0.0, size);
-                    let colors = [hex("5c5c5c"),hex("747575"), hex("949494"), hex("aba9a9"), hex("c2c0c0"),hex("dbd9d9"),hex("edebeb"),hex("ffffff")];
-                    let mut size = (body.pos.2/100.).ceil() as usize;
-                    if size == 0 {
-                        size = 1;
-                    }
-                    ellipse(colors[size-1], circle, transform, gl);
+                let transform = c.transform.trans(body.pos.0, body.pos.1);
+
+                let size = (body.pos.2 / 80.).ceil();
+                let circle = ellipse::circle(0.0, 0.0, size);
+                let colors = [
+                    hex("5c5c5c"),
+                    hex("747575"),
+                    hex("949494"),
+                    hex("aba9a9"),
+                    hex("c2c0c0"),
+                    hex("dbd9d9"),
+                    hex("edebeb"),
+                    hex("ffffff"),
+                ];
+                let mut size = (body.pos.2 / 100.).ceil() as usize;
+                if size == 0 {
+                    size = 1;
+                }
+                ellipse(colors[size - 1], circle, transform, gl);
             }
         });
     }
@@ -48,7 +53,7 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("N-Body-Sim", [YMAX, XMAX])
+    let mut window: Window = WindowSettings::new("N-Body-Sim", [XMAX, YMAX])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -64,9 +69,8 @@ fn main() {
 
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
-            sim.render(&args,&nbodies);
+            sim.render(&args, &nbodies);
             nbodies.update_with_tree();
-            println!("Looped!");
         }
     }
 }
